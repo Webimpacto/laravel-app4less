e0033a242fdb2e51e330ec749aa5f982505f5971
Pablo Martin <pmartin@webimpacto.es>
2018-05-08 15:01:50 +0200

    First Commit

---
+++ src/App4LessServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+namespace Webimpacto\LaravelApp4Less;
+use Illuminate\Support\ServiceProvider;
+class App4LessServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'../migrations/');
+    }
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
